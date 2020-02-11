4290cac053483f777b0f52de5100e210001d7500
levekyle <kleve@andrew.cmu.edu>
2020-02-11 14:39:39 -0500

    Part 1 Phase 1

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -5,4 +5,8 @@ input_dat_file = "data/pfgd_test.dat"
 
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+
+input_dat_file = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+
 #print the resulting data
+print(input_dat_file)
